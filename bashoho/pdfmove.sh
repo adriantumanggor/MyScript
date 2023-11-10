9e546908853a58b20f3304071f8b04209b18bad7
adrian <mendienta990@gmail.com>
2023-11-10 15:34:38 +0700

    .

--- bashoho/pdfmove.sh
+++ bashoho/pdfmove.sh
@@ -5,7 +5,7 @@ target_folder="/home/tumanggor675/Downloads/pdf"
 
 counter=1
 
-# Loop untuk setiap file dengan ekstensi .jpg atau .jpeg di dalam folder unduhan
+# Loop untuk setiap file dengan ekstensi .pdf
 for file_path in "$download_folder"/*.pdf ; do
     # Memeriksa keberadaan file dengan nama yang sama di dalam folder tujuan
     while [ -e "$target_folder/$(basename "$file_path")" ]; do
@@ -14,7 +14,7 @@ for file_path in "$download_folder"/*.pdf ; do
         ((counter++))
     done
 
-    # Jika tidak ada konflik, pindahkan file langsung
+    # pindahkan file 
     mv "$file_path" "$target_folder/"
     echo "File $(basename "$file_path") berhasil dipindahkan ke $target_folder/"
 done
