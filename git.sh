eb08f58245f8afaf18a62cb49de3467ba32a4837
ucok <mendienta990@gmail.com>
2024-07-12 08:19:59 +0700

    Default commit message

--- git.sh
+++ git.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-# Set the commit message
-COMMIT_MESSAGE="${1:-Default}"
+# Prompt the user to specify files to add, or press enter to add all
+echo "Enter the files to add to the staging area (press enter to add all):"
+read FILES_TO_ADD
 
-# Add all changes to the staging area
-git add --all
+# If no files are specified, add all changes
+if [ -z "$FILES_TO_ADD" ]; then
+    echo "No specific files entered. Adding all changes to the staging area..."
+    git add --all
+else
+    echo "Adding specified files to the staging area..."
+    git add $FILES_TO_ADD
+fi
+
+# Set the commit message
+COMMIT_MESSAGE="${1:-Default commit message}"
 
 # Commit with the provided message
+echo "Committing changes with message: '$COMMIT_MESSAGE'"
 git commit -m "$COMMIT_MESSAGE"
 
 # Attempt to push changes to GitHub
+echo "Pushing changes to GitHub..."
 git push
 
 if [ $? -ne 0 ]; then
