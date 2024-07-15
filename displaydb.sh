6b0778cf7c69ab4aeebdbc4739be1524f23a142c
ucok <mendienta990@gmail.com>
2024-07-15 14:32:18 +0700

    Default commit message

--- displaydb.sh
+++ displaydb.sh
@@ -1,35 +1,4 @@
-#!/bin/bash
-
-# Database credentials
-USER="YOUR_USERNAME"
-PASSWORD="YOUR_PASSWORD"
-
-
-# Prompt the user for the type of query
-echo "Select query type:"
-echo "1) Display databases"
-echo "2) Display database structure"
-echo "3) Display table structure"
-read -p "Enter your choice (1, 2, or 3): " choice
-
-case $choice in
-  1)
-    QUERY="SHOW DATABASES;"
-    ;;
-  2)
-    read -p "Enter the database name: " DB
-    QUERY="USE $DB; SHOW TABLES;"
-    ;;
-  3)
-    read -p "Enter the database name: " DB
-    read -p "Enter the table name: " TABLE
-    QUERY="USE $DB; DESCRIBE $TABLE;"
-    ;;
-  *)
-    echo "Invalid choice"
-    exit 1
-    ;;
-esac
-
-# Execute the query and print the output
-sudo mysql -u $USER -p$PASSWORD -e "$QUERY"
+USER="root"
+PASSWORD="root"
+  QUERY="SHOW DATABASES;"
+  sudo mysql -u $USER -p$PASSWORD -e "$QUERY"
